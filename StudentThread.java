5fe982b7b80fe2f896a5693c15b364180927d817
JamesDeciutiis <46979777+James-Deciutiis@users.noreply.github.com>
2020-11-17 12:54:01 -0800

    11/17/20

--- StudentThread.java
+++ StudentThread.java
@@ -1,41 +1,55 @@
 import java.util.Random;
 public class StudentThread extends Thread{
 	
-	public static final int sleepTime = 10000;
+	public static final int GIRL = 0;
+	public static final int BOY = 1;
+	public static final int MAX_SLEEP_TIME = 10000;
+	public static long time = System.currentTimeMillis();
 	private String Student_Id;
+	private int gender;
 	private int busy = 0;
+	private int waitingForBathroom = 0;
+	private int doneWithBathroom = 0;
 
 	public StudentThread(String id){
 		this.Student_Id = id;
-		System.out.println("busy " + this.busy);
+		this.gender = getRandomGender();
 	}
 
 	public void run(){
 		//Student thread created
-		System.out.println("Student " + this.Student_Id + " woken up!");
+		this.printMessage(" woken up!");
 
 		//Health questionaire
-		this.sleepMessage("Student " + this.Student_Id + " is filling out health questionaire");
+		this.sleepMessage(" is filling out health questionaire");
 		
 		//Student commutes to school
-		this.sleepMessage("Student " + this.Student_Id + " is commuting to school");
+		this.sleepMessage(" is commuting to school");
 		
-		System.out.println("Student " + this.Student_Id + " waiting in the schoolyard to be called by teacher");
+		this.printMessage(" waiting in the schoolyard to be called by teacher");
 		this.busy++;
-		while(busy == 1){
-			//wait in schoolyard
+		while(this.busy == 1){/*wait in schoolyard*/}
+		this.printMessage(" has been let in by the teacher");
 
+		//waiting for bathroom
+		this.printMessage(" waiting for bathroom");
+		this.waitingForBathroom++;
+		while(this.waitingForBathroom == 1){
+			this.yield();
+			this.yield();
+			this.yield();
 		}
-
+		this.sleepMessage(" is using the bathroom");
+		this.doneWithBathroom++;
 
 		//Student finishes day
-		System.out.println("Student " + this.Student_Id + " done");
+		this.printMessage(" is done");
 	}
 	
 	public void sleepMessage(String msg){
 		try{
-			System.out.println(msg);
-			this.sleep(sleepTime);
+			System.out.println("[" + (System.currentTimeMillis() - time) + "] " + "Student " + this.Student_Id + msg);
+			this.sleep(this.getRandomSleepTime());
 		}
 		catch(InterruptedException e){
 
@@ -43,8 +57,26 @@ public class StudentThread extends Thread{
 	}
 
 	public void interruptMessage(String msg){
-		System.out.println(msg);
+		System.out.println("[" + (System.currentTimeMillis() - time) + "] " + "Student " + this.Student_Id + msg);
 		this.interrupt();
+	}
+	
+	public void printMessage(String msg){
+		System.out.println("[" + (System.currentTimeMillis() - time) + "] " + "Student " + this.Student_Id + msg);
+	}
+
+	public int getRandomSleepTime(){
+		Random rand = new Random();
+		int retval = rand.nextInt(MAX_SLEEP_TIME);
+
+		return retval;
+	}
+	
+	public int getRandomGender(){
+		Random rand = new Random();
+		int retval = rand.nextInt(BOY);
+
+		return retval;
 	}
 	
 	public int getBusy(){
@@ -53,5 +85,25 @@ public class StudentThread extends Thread{
 
 	public void setBusy(int busy){
 		this.busy = busy;
+	}
+	
+	public int getWaitingForBathroom(){
+		return this.waitingForBathroom;
+	}
+
+	public void setWaitingForBathroom(int waiting){
+		this.waitingForBathroom = waiting;
+	}
+	
+	public int getGender(){
+		return this.gender;
+	}
+
+	public int getDoneWithBathroom(){
+		return this.doneWithBathroom;
+	}
+
+	public void setDoneWithBathroom(int done){
+		this.doneWithBathroom = done;
 	}
 }
--- TeacherThread.java
+++ TeacherThread.java
@@ -1,10 +1,19 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class TeacherThread extends Thread{
 	
-	private int classSize = 1;
+	public static final int GIRL = 0;
+	public static final int BOY = 1;
+	private int classSize = 6;
+	private int girlsBathroomCount = 0;
+	private int boysBathroomCount = 0;
 	private StudentThread [] students;
+	private int [] bathroomQueue;
+	AtomicBoolean atomicBoolean = new AtomicBoolean(true); 
 
 	public TeacherThread(){
 		students = new StudentThread[classSize];
+		bathroomQueue = new int[classSize];
 
 		for(int i = 0; i < classSize; i++){
 			students[i] = new StudentThread(Integer.toString(i));
@@ -14,10 +23,52 @@ public class TeacherThread extends Thread{
 			students[i].start();
 		}
 		
-		students[0].setBusy(0);
-		while(students[0].getBusy() == 1){
-		}
+		int i = 0;
+		while(atomicBoolean.get()){
+			for(int j = 0; j < classSize; j++){
+				//checks if student is busy waiting in schoolyard, then teacher lets them in
+				//students are put into the bathroom queue based on the order they were let into the school
+				if(students[j].getBusy() == 1){
+					students[j].setBusy(0);
+					bathroomQueue[i] = j;
+				}
+			}
+
+			for(int j = 0; j < classSize; j++){
+				if(students[bathroomQueue[j]].getWaitingForBathroom() == 1 && students[bathroomQueue[j]].getGender() == BOY){
+					//if theres room in boys bathroom let boy go to the bathroom
+					if(boysBathroomCount < 2){
+						students[bathroomQueue[j]].setWaitingForBathroom(0);
+						boysBathroomCount++;
+					}
+				}
+				
+				else if(students[bathroomQueue[j]].getWaitingForBathroom() == 1 && students[bathroomQueue[j]].getGender() == GIRL){
+					//if theres room in boys bathroom let boy go to the bathroom
+					if(boysBathroomCount < 2){
+						students[bathroomQueue[j]].setWaitingForBathroom(0);
+						girlsBathroomCount++;
+					}
+				}
+
+				//if student is done with the bathroom, free up a spot in the bathroom and move on
+				if(students[bathroomQueue[j]].getDoneWithBathroom() == 1 && students[bathroomQueue[j]].getGender() == BOY){
+					boysBathroomCount--;
+					students[bathroomQueue[j]].setDoneWithBathroom(0);
+					i++;
+				}
+				else if(students[bathroomQueue[j]].getDoneWithBathroom() == 1 && students[bathroomQueue[j]].getGender() == GIRL){
+					girlsBathroomCount--;
+					students[bathroomQueue[j]].setDoneWithBathroom(0);
+					i++;
+				}
+			}
 			
+			if(i >= classSize){
+				atomicBoolean.set(false);
+			}
+		}
+		
 		System.out.println("TEST TEST TEST TEST");
 	}
 	
